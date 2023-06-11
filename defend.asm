504dfe1660adb605c16f64da2305af735ab74195
CellBro <542739645@qq.com>
2023-06-11 16:49:14 +0800

    修改提示

--- defend.asm
+++ defend.asm
@@ -133,7 +133,7 @@ kill endp
     dta        db     02bh dup(0)
     string     db     "I'm a kill!",13,10,'$'
     head       db     30h dup(0)
-    warnning   db     "virus is Detected！！", 0dh,0ah,24h
+    warnning   db     "virus is detected", 0dh,0ah,24h
     theend:    
 code ends
 end start
